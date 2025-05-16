a3ffed7dfdcba3ad72ca28ad7b2a1c3e54d9731c
Andre da Silva <adasi5@uis.edu>
2025-05-16 13:51:57 -0400

    basic functional components

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
+                                                                    /*import logo from './logo.svg';*/
 import './App.css';
 import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
 /*const name = "Andre";*/
 
-const handleNameChange = () =>{
-const names = ['Bob', 'Kevin', 'Andre'];
-const int = Math.floor(Math.random() * 3);
-return names[int];
-}
   return (
     <div className="App">
       <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
